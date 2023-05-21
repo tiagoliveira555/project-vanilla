0919a531c9865c6840313baff2f338a07032401a
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-05-21 20:53:37 -0300

    configured icon insert-card

--- js/views/menu-card.js
+++ js/views/menu-card.js
@@ -3,19 +3,36 @@ import { Main } from "./main.js"
 
 export class MenuCard {
   constructor() {
+    this.init()
+  }
+
+  init() {
     this.element = document.createElement('div')
     this.element.className = 'main'
     const h1 = document.createElement('h1')
     h1.textContent = 'Insira seu cartão de transporte'
     const div = document.createElement('div')
     div.className = 'menu-card'
+    div.appendChild(this.addInsertCard())
+    this.element.appendChild(h1)
+    this.element.appendChild(div)
+    this.element.appendChild(this.createButtonBack())
+  }
+
+  addInsertCard() {
+    const insertCard = document.createElement('div')
+    insertCard.className = 'insert-card'
+    const span = document.createElement('span')
+    insertCard.appendChild(span)
+    return insertCard
+  }
+
+  createButtonBack() {
     const divButtons = document.createElement('div')
     divButtons.className = 'buttons'
     const buttonBack = new Button('Voltar', 'button-back')
     divButtons.appendChild(buttonBack.element)
-    this.element.appendChild(h1)
-    this.element.appendChild(div)
-    this.element.appendChild(divButtons)
     buttonBack.addListener(() => this.element.replaceWith(new Main().element))
+    return divButtons
   }
 }
