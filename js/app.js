26a19a19f9f2e71e1ce27080f7bc1b3d9d7a07a6
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-05-21 16:23:55 -0300

    create button menu

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@ const app = document.querySelector('#app')
 const main = new Main()
 const footer = new Footer()
 
-app.appendChild(main.element)
+// app.appendChild(main.element)
 app.appendChild(footer.element)
