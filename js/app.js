1983996710bbd64f138f599d19aa7ebf1dace2bc
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-05-21 11:57:40 -0300

    intial project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const app = document.querySelector('#app')
+
+const main = new Main()
+const footer = new Footer()
+
+app.appendChild(main.element)
+app.appendChild(footer.element)
---
+++ js/components/main.js
@@ -0,0 +1,7 @@
+class Main {
+  constructor() {
+    this.element = document.createElement('div')
+    this.element.className = 'main'
+    this.element.innerHTML = '<h1>Página principal</h1>'
+  }
+}
